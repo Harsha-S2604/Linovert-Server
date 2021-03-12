08bdc4133ea39d60f16ef80022fd1c33e567288a
Harsha-S2604 <harsha91148@gmail.com>
2021-03-12 20:31:48 +0530

    added new mongodb connection

--- src/db/connectDB.go
+++ src/db/connectDB.go
@@ -14,17 +14,19 @@ var (
 )
 
 func ConnectDB() {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27018/")
+	clientOptions := options.Client().ApplyURI("mongodb+srv://arix2406:6Dwy4jq8h4s26us20@linovert.ul3hz.mongodb.net/LinovertDB?retryWrites=true&w=majority")
 	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
 		log.Println("Sorry something went wrong. Couldn't connect to database")
-	}
-	err = client.Ping(ctx, nil)
-	if err != nil {
-		log.Fatal("Sorry something went wrong. Couldn't connect to database")
 	} else {
-		log.Println("Successfully connected to database :)")
+		log.Println("Successfully connected to the database :)")
 	}
+	// err = client.Ping(ctx, nil)
+	// if err != nil {
+	// 	log.Fatal("Sorry something went wrong. Couldn't connect to database")
+	// } else {
+	// 	log.Println("Successfully connected to database :)")
+	// }
 
 	Database = client.Database("LinovertDB")
 }
